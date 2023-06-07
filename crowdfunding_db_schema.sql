65e569c9cfac4f2cfa5304c5e5f4e10f1c531891
PsCushman <122395437+PsCushman@users.noreply.github.com>
2023-06-07 10:13:39 -0700

    fixed data types and added screen shots

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,12 +2,12 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE category (
-    category_id VARCHAR(4) PRIMARY KEY,
+    category_id CHAR(4) PRIMARY KEY,
     category VARCHAR(25)
 );
 
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR(7) PRIMARY KEY,
+    subcategory_id VARCHAR(8) PRIMARY KEY,
     subcategory VARCHAR(25)
 );
 
@@ -22,17 +22,17 @@ CREATE TABLE campaign (
     cf_id INT PRIMARY KEY,
     contact_id INT,
     company_name VARCHAR(55),
-    description TEXT,
+    description VARCHAR(255),
     goal DECIMAL(10, 2),
     pledged DECIMAL(10, 2),
     outcome VARCHAR(35),
     backers_count INT,
-    country VARCHAR(2),
-    currency VARCHAR(3),
+    country CHAR(2),
+    currency CHAR(3),
     launch_date DATE,
     end_date DATE,
-    category_id VARCHAR(4),
-    subcategory_id VARCHAR(7),
+    category_id CHAR(4),
+    subcategory_id VARCHAR(8),
     FOREIGN KEY (category_id) REFERENCES category(category_id),
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
     FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
